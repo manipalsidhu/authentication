c7c84126b402127a0dd8fb1bfbd2e9db7d3a8b40
manipalsidhu <manipals@yahoo.com>
2021-10-02 21:54:30 -0400

    final Commit

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('users', { 
+    title: 'Users',
+    userName: 'Manipal Singh Sidhu'
+   });
+});
+
+module.exports = router;
