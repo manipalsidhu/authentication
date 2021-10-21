7e36208bf2b48f32b312905d93d34e074cced77e
manipalsidhu <manipals@yahoo.com>
2021-10-21 12:34:14 -0400

    final update

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/Business_Contacts_List"
+   // "URI": "mongodb://localhost/Business_Contacts_List"
+    "URI": "mongodb+srv://mani:x638mrFfD3v161cs@mongodbserver.wpmmb.mongodb.net/Business_Contacts_List?retryWrites=true&w=majority"
 }
